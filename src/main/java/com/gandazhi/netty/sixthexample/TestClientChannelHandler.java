d55f441b7146eebfd59f21f7ca3fc6b002b24a61
gandazhi <gandazhi@gmail.com>
2018-12-09 15:47:44 +0800

    使用protobuf 客户端向服务端发送不同的消息类型

--- src/main/java/com/gandazhi/netty/sixthexample/TestClientChannelHandler.java
+++ src/main/java/com/gandazhi/netty/sixthexample/TestClientChannelHandler.java
@@ -1,23 +1,54 @@
 package com.gandazhi.netty.sixthexample;
 
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.Random;
 
 /**
  * @Auther: gandazhi
  * @Date: 2018-12-03 20:52
  */
-public class TestClientChannelHandler extends SimpleChannelInboundHandler<MyDataInfo.MyPerson> {
+public class TestClientChannelHandler extends SimpleChannelInboundHandler<MyDataInfo.MyMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyPerson msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyMessage msg) throws Exception {
 
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        MyDataInfo.MyPerson myPerson = MyDataInfo.MyPerson.newBuilder().setName("gandazhi").setAge(20).setAddress("chengdu").build();
 
-        ctx.channel().writeAndFlush(myPerson);
+        int i = new Random().nextInt(3);
+        MyDataInfo.MyMessage myMessage = null;
+        switch (i) {
+            case 0:
+                MyDataInfo.MyPerson myPerson = MyDataInfo.MyPerson.newBuilder().setName("gandazhi").setAge(20).setAddress("chengdu").build();
+
+                myMessage = MyDataInfo.MyMessage.newBuilder()
+                        .setDataType(MyDataInfo.MyMessage.DataType.PersonType)
+                        .setPerson(myPerson)
+                        .build();
+                break;
+            case 1:
+                MyDataInfo.Dog dog = MyDataInfo.Dog.newBuilder().setName("dog").setAge(2).build();
+
+                myMessage = MyDataInfo.MyMessage.newBuilder()
+                        .setDataType(MyDataInfo.MyMessage.DataType.DogType)
+                        .setDog(dog)
+                        .build();
+                break;
+            case 2:
+                MyDataInfo.Cat cat = MyDataInfo.Cat.newBuilder().setName("cat").setCity("chengdu").build();
+
+                myMessage = MyDataInfo.MyMessage.newBuilder()
+                        .setDataType(MyDataInfo.MyMessage.DataType.CatType)
+                        .setCat(cat)
+                        .build();
+                break;
+        }
+
+        ctx.channel().writeAndFlush(myMessage);
     }
 }
--- src/main/java/com/gandazhi/netty/sixthexample/TestServerChannelHandler.java
+++ src/main/java/com/gandazhi/netty/sixthexample/TestServerChannelHandler.java
@@ -7,12 +7,27 @@ import io.netty.channel.SimpleChannelInboundHandler;
  * @Auther: gandazhi
  * @Date: 2018-12-03 20:41
  */
-public class TestServerChannelHandler extends SimpleChannelInboundHandler<MyDataInfo.MyPerson> {
+public class TestServerChannelHandler extends SimpleChannelInboundHandler<MyDataInfo.MyMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyPerson msg) throws Exception {
-        System.out.println(msg.getAddress());
-        System.out.println(msg.getAge());
-        System.out.println(msg.getName());
+    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyMessage msg) throws Exception {
+        switch (msg.getDataType()) {
+            case PersonType:
+                System.out.println(msg.getPerson().getName());
+                System.out.println(msg.getPerson().getAge());
+                System.out.println(msg.getPerson().getAddress());
+                break;
+            case CatType:
+                System.out.println(msg.getCat().getName());
+                System.out.println(msg.getCat().getCity());
+                break;
+            case DogType:
+                System.out.println(msg.getDog().getName());
+                System.out.println(msg.getDog().getAge());
+                break;
+            default:
+                System.out.println("传输dataType错误");
+                break;
+        }
     }
 }
